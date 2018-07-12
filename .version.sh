bd70a52fb9b2cd1bbd18a5302a977c419daaaa0e
Allen Wild <allenwild93@gmail.com>
2018-07-12 12:11:50 -0400

    version.sh: force dev versioning if current branch is master
    Since the 1.5.0-rc1 tag is actually in master unlike previous release
    RC tags.

--- .version.sh
+++ .version.sh
@@ -2,13 +2,13 @@
 
 codeversion=`sed -n 's/^var Version.*"\([^"]\+\).*/\1/p' main.go`
 gitversion=`git describe --tags --abbrev=0 | sed 's/v\([^-]\+\).*/\1/'`
+branch=`git symbolic-ref HEAD 2>/dev/null | sed -n 's:^refs/heads/::p'` || true
 
-if ( echo "$codeversion" | grep -q "$gitversion" ); then
+if [[ $branch != master ]] && ( echo "$codeversion" | grep -q "$gitversion" ); then
     # on a release branch use the usual format
     git describe --tags --always --dirty=+ | sed 's/-/+/; s/^v//'
 else
     # working off master, use branch name and rev count
-    branch=`git symbolic-ref HEAD 2>/dev/null | sed -n 's:^refs/heads/::p'` || true
     [ -z "$branch" ] || branch="-$branch"
     sha=`git rev-parse --short HEAD`
     revcount=`git rev-list HEAD | wc -l`
