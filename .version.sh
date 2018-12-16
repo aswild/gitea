f188732742bd7576a5463845295e4ee64500b0e6
Allen Wild <allenwild93@gmail.com>
2018-12-16 17:29:24 -0500

    version.sh: remove bashisms
    Apparently the new go 1.11.3 alpine build container doesn't have bash
    installed, giving an error "sh: *master*: unknown operand", and if
    I #!/bin/bash then it can't run at all.
    
    Use an old fashioned echo | grep instead.

--- .version.sh
+++ .version.sh
@@ -1,10 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
 
 codeversion=`sed -n 's/^var Version.*"\([^"]\+\).*/\1/p' main.go`
 gitversion=`git describe --tags --abbrev=0 | sed 's/v\([^-]\+\).*/\1/'`
 branch=`git symbolic-ref HEAD 2>/dev/null | sed -n 's:^refs/heads/::p'` || true
 
-if [[ $branch != *master* ]]; then
+if ! (echo "$branch" | grep master >/dev/null 2>&1); then
     # on a release branch use the usual format
     tag="$(git describe --tags --always --abbrev=0)"
     git describe --tags --always --dirty=+ | sed "s/${tag}-/${tag}+/; s/^v//"
