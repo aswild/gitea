2a7e90e79ec7b5d3ce139d0a49b3c671664ae36a
Allen Wild <allenwild93@gmail.com>
2019-01-14 00:29:11 -0500

    contrib: add script to run integration tests
    Automate the process of creating/removing the postgres db container.

---
+++ contrib/run-integration-pgsql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+POSTGRES_USER=giteaIntegration
+POSTGRES_PASSWORD=giteaIntegrationPassword
+POSTGRES_DB=giteaIntegration
+
+container_opts=(
+    -e POSTGRES_USER="$POSTGRES_USER"
+    -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD"
+    -e POSTGRES_DB="$POSTGRES_DB"
+)
+
+die() {
+    echo "$*" >&2
+    exit 1
+}
+
+stop_container() {
+    echo "Stopping postgres"
+    docker container stop $container_id
+}
+
+echo "Starting postgres"
+container_id="$(docker run -d --rm "${container_opts[@]}" postgres)"
+[[ -n $container_id ]] || die "failed to create postgres container"
+trap stop_container EXIT
+
+container_ip="$(docker container inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)"
+[[ -n $container_ip ]] || die "failed to get postgres container ip"
+
+test_opts=(
+    TEST_PGSQL_HOST="$container_ip"
+    TEST_PGSQL_DBNAME="$POSTGRES_DB"
+    TEST_PGSQL_USERNAME="$POSTGRES_USER"
+    TEST_PGSQL_PASSWORD="$POSTGRES_PASSWORD"
+)
+
+echo make "${test_opts[@]}" test-pgsql
+make "${test_opts[@]}" test-pgsql
